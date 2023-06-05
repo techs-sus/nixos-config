9669cccf8ce0029637920521e638f38412bdd8e0
techs-sus <92276908+techs-sus@users.noreply.github.com>
2023-06-04 20:58:20 -0400

    dev: fix

--- configuration.nix
+++ configuration.nix
@@ -24,7 +24,6 @@
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  sys.use_memfd = true;
   # Set your time zone.
   time.timeZone = "America/New_York";
 
